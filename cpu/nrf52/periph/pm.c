5ba404bf9ebfdb4f0884475c17ab13b84f69389f
William Takahashi <william.ryort@gmail.com>
2022-10-17 17:25:33 -0300

    Initial configs for pm_layered

---
+++ cpu/nrf52/periph/pm.c
@@ -0,0 +1,19 @@
+#include "periph/pm.h"
+#include "periph_cpu.h"
+#include "pm_layered.h"
+
+
+
+void pm_set(unsigned mode)
+{
+    switch (mode) {
+        case 0:
+            //Implementar o power mode 0
+        break;
+        case 1:
+            //Implementar o power mode 1
+        break;
+    }
+}
+
+void pm_off(void);
